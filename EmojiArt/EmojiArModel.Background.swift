56a3490c26c1db260a6ea880d3090b192d9ff02f
Karlis <karlis.developer@gmail.com>
2022-01-19 11:46:29 +0200

    Finish Lecture 9 - Emoji Drag and Drop

---
+++ EmojiArt/EmojiArModel.Background.swift
@@ -0,0 +1,31 @@
+//
+//  EmojiArModel.Background.swift
+//  EmojiArt
+//
+//  Created by karlis.stekels on 19/01/2022.
+//
+
+import Foundation
+
+extension EmojiArtModel {
+    enum Background: Equatable {
+        case blank
+        case url(URL)
+        case imageData(Data)
+        
+        var url: URL? {
+            switch self {
+            case .url(let url): return url
+            default: return nil
+            }
+        }
+        
+        var imageData: Data? {
+            switch self {
+            case .imageData(let data): return data
+            default: return nil
+            }
+        }
+    }
+}
+
