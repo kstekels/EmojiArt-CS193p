77e846a4c04d340d521250e2ad94d6c480dfc07f
Karlis <karlis.developer@gmail.com>
2022-01-19 09:45:06 +0200

    Initial Commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by karlis.stekels on 19/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
